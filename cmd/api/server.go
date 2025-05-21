3c4a52f8c535456aa60e0bd53725362e270f5e1f
ravigill3936 <gill1234ravi@gmail.com>
2025-05-20 23:07:37 -0400

    learned something new today

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -5,7 +5,8 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net/http"
-	"time"
+
+	// "time"
 
 	"golang.org/x/net/http2"
 )
@@ -13,25 +14,28 @@ import (
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the root endpoint\n")
 }
-
-func userHandler(w http.ResponseWriter, r *http.Request) {
+func teachersHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		fmt.Fprintln(w, "GET: Returning user info")
+		fmt.Fprintln(w, "GET: Returning teacher info")
+
 	case http.MethodPost:
-		fmt.Println(r.URL.Query())
-		fmt.Println(r.URL.Query().Get("name"))
+		fmt.Fprintln(w, "POST: Creating a new teacher")
 
-		//parse from data
+	case http.MethodPut:
+		fmt.Fprintln(w, "PUT: Replacing teacher details")
 
-		err := r.ParseForm()
-		if err != nil {
-			return
-		}
+	case http.MethodPatch:
+		fmt.Fprintln(w, "PATCH: Updating partial teacher data")
 
-		fmt.Println(r.Form)
+	case http.MethodDelete:
+		fmt.Fprintln(w, "DELETE: Removing teacher record")
 
-		w.Write([]byte("Post method in execution"))
+	case http.MethodOptions:
+		// Inform client of supported methods
+		w.Header().Set("Allow", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.WriteHeader(http.StatusNoContent)
+
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
@@ -42,7 +46,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
-	mux.HandleFunc("/user", userHandler)
+	mux.HandleFunc("/user", teachersHandler)
 
 	cert := "cert.pem"
 	key := "key.pem"
@@ -51,11 +55,22 @@ func main() {
 		MinVersion: tls.VersionTLS12,
 	}
 
-	rl := mw.NewRateLimiter(5, time.Minute)
+	// rl := mw.NewRateLimiter(5, time.Minute)
 
+	// hppOptions := mw.HPPOptions{
+	// 	CheckQuery:              true,
+	// 	CheckBody:               true,
+	// 	CheckBodyForContentType: "application/x-www-form-urlencoded",
+	// 	Whitelist:               []string{"sortBy", "sortOrder", "name", "age", "class"},
+	// }
+
+	// secureMux := mw.Cors(rl.Middlerware(mw.ResponseTimeMiddlerware(mw.Compression(mw.Hpp(hppOptions)(mw.SecurityHeader(mux))))))
+	// secureMux := applyMiddleware(mux, mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeader, mw.ResponseTimeMiddlerware, rl.Middlerware, mw.Cors)
+
+	secureMux := mw.SecurityHeader(mux)
 	server := &http.Server{
 		Addr:      fmt.Sprintf(":%d", port),
-		Handler:   rl.Middlerware(mw.Compression(mw.ResponseTimeMiddlerware(mw.SecurityHeader(mw.Cors(mux))))),
+		Handler:   secureMux,
 		TLSConfig: tlsConfig,
 	}
 
@@ -67,4 +82,14 @@ func main() {
 	if err != nil {
 		fmt.Println("Error starting the server:", err)
 	}
+}
+
+// Middleware is a fn  that wraps on http.handler with addiotanial fntionality
+type Middlerware func(http.Handler) http.Handler
+
+func applyMiddleware(handler http.Handler, middlerwares ...Middlerware) http.Handler {
+	for _, middlerware := range middlerwares {
+		handler = middlerware(handler)
+	}
+	return handler
 }
